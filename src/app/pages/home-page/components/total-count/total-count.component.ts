f9daf7b6dbec53349b80386251e2a70976059c89
Anita Rosikhina <anita.rosikhina@hebronsoft.com>
2023-08-03 06:47:32 +0300

    implemented TotalCountComponent

--- src/app/pages/home-page/components/total-count/total-count.component.ts
+++ src/app/pages/home-page/components/total-count/total-count.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-total-count',
   templateUrl: './total-count.component.html',
-  styleUrls: ['./total-count.component.scss']
+  styleUrls: ['./total-count.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TotalCountComponent {
-
+  @Input() count?: number;
 }
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HomePageComponentsModule } from "./home-page-components.module";
+import { MatDividerModule } from "@angular/material/divider";
 
 @Component({
   standalone: true,
   imports: [
-    HomePageComponentsModule
+    HomePageComponentsModule,
+    MatDividerModule
   ],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
